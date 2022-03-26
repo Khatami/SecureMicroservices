62d9b3e5ff36a1bd1cfcb2f72f421188f9e72de9
hamedkhatami <hamedkhatami@outlook.com>
2022-03-26 18:51:16 +0430

    feat (Movies.API): add MovieController and MovieDbContext

--- src/Services/Movies.API/Program.cs
+++ src/Services/Movies.API/Program.cs
@@ -1,13 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Movies.API.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<MoviesDbContext>(options =>
+{
+	options.UseInMemoryDatabase("Movies");
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+using var context = scope.ServiceProvider.GetService<MoviesDbContext>();
+context.Database.EnsureCreated();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
