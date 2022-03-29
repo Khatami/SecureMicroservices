d108fa983b276ae4e95115a16ec5a57af201ca5c
hamedkhatami <hamedkhatami@outlook.com>
2022-03-29 21:23:08 +0430

    feat (IdentityServer): add QuickUI and add a new interactive client

--- src/Services/IdentityServer/IdentityServer/Config.cs
+++ src/Services/IdentityServer/IdentityServer/Config.cs
@@ -1,5 +1,8 @@
-﻿using IdentityServer4.Models;
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
 using IdentityServer4.Test;
+using System.Security.Claims;
 
 namespace IdentityServer
 {
@@ -7,15 +10,42 @@ namespace IdentityServer
 	{
 		public static IEnumerable<Client> Clients => new Client[]
 		{
+			// Machine To Machine Communication
 			new Client()
 			{
 				ClientId = "movieClient",
-				AllowedGrantTypes = { GrantType.ClientCredentials }, //TODO: ?
+				AllowedGrantTypes = GrantTypes.ClientCredentials,
+				ClientSecrets =
+				{
+					new Secret("secret".Sha256())
+				},
+				AllowedScopes = { "movieAPI" }
+			},
+
+			// Interactive Clients
+			new Client()
+			{
+				ClientId = "movies_mvc_clinet",
+				ClientName = "Movies MVC Web App",
+				AllowedGrantTypes = GrantTypes.Code,
+				AllowRememberConsent = false, //TODO: ?
+				RedirectUris = new List<string>()
+				{
+					"https://localhost:6600/signin-oidc"
+				},
+				PostLogoutRedirectUris = new List<string>()
+				{
+					"https://localhost:6600/signout-callback-oidc"
+				},
 				ClientSecrets =
 				{
 					new Secret("secret".Sha256())
 				}, //TODO: ?
-				AllowedScopes = { "movieAPI" }
+				AllowedScopes = new List<string>()
+				{
+					IdentityServerConstants.StandardScopes.OpenId,
+					IdentityServerConstants.StandardScopes.Profile
+				}
 			}
 		};
 
@@ -30,10 +60,23 @@ namespace IdentityServer
 
 		public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
 		{
+			//new IdentityResources.OpenId(), //TODO: ?
+			//new IdentityResources.Profile() //TODO: ?
 		};
 
 		public static List<TestUser> TestUsers => new List<TestUser>
 		{
+			new TestUser()
+			{
+				SubjectId = "1A2A8CF9-4533-4442-92CB-1D22E198F6CA",
+				Username = "hamed",
+				Password = "hamed",
+				Claims = new List<Claim>()
+				{
+					new Claim(JwtClaimTypes.GivenName, "Seyedhamed"),
+					new Claim(JwtClaimTypes.FamilyName, "Khatami")
+				}
+			}
 		};
 	}
 }
--- src/Services/IdentityServer/IdentityServer/Program.cs
+++ src/Services/IdentityServer/IdentityServer/Program.cs
@@ -2,27 +2,33 @@ using IdentityServer;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// MVC
+builder.Services.AddControllersWithViews();
+
 // IdentityServer
 builder.Services
 	.AddIdentityServer()
 	.AddInMemoryClients(Config.Clients)
-	//.AddInMemoryIdentityResources(Config.IdentityResources)
-	//.AddInMemoryApiResources(Config.ApiResources)
+	.AddInMemoryIdentityResources(Config.IdentityResources)
+	.AddInMemoryApiResources(Config.ApiResources)
 	.AddInMemoryApiScopes(Config.ApiScopes)
-	//.AddTestUsers(Config.TestUsers)
+	.AddTestUsers(Config.TestUsers)
 	.AddDeveloperSigningCredential();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+app.UseStaticFiles();
+
 // IdentityServer
 app.UseRouting();
 app.UseIdentityServer();
+app.UseAuthorization();
 
-app.MapGet("/", async handler =>
-{
-	await handler.Response.WriteAsync("IdentityServer");
-});
+// MVC
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
