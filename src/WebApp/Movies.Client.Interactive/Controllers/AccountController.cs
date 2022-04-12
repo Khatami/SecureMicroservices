e61b74c7be117327b8f2b45a2bd46680d3470f7b
hamedkhatami <hamedkhatami@outlook.com>
2022-04-12 20:37:10 +0430

    feat: add AccessDenied view

---
+++ src/WebApp/Movies.Client.Interactive/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Movies.Client.Interactive.Controllers
+{
+	[Authorize]
+	public class AccountController : Controller
+	{
+		public IActionResult AccessDenied()
+		{
+			return View();
+		}
+	}
+}
