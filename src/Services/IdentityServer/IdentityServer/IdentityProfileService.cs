4735fdd3aaed4aaaa1f8d4272b84e57d30b31a11
hamedkhatami <hamedkhatami@outlook.com>
2022-04-18 12:13:47 +0430

    fix (IdentityServer): check context.caller for populating RequestClaimTypes

--- src/Services/IdentityServer/IdentityServer/IdentityProfileService.cs
+++ src/Services/IdentityServer/IdentityServer/IdentityProfileService.cs
@@ -9,10 +9,15 @@ namespace IdentityServer
 		public async Task GetProfileDataAsync(ProfileDataRequestContext context)
 		{
 			var claimsNames = new List<string>();
-			claimsNames.AddRange(context.RequestedResources.Resources.IdentityResources.SelectMany(r => r.UserClaims));
-			claimsNames.AddRange(context.RequestedResources.Resources.ApiResources.SelectMany(r => r.UserClaims));
-			claimsNames.AddRange(context.RequestedResources.Resources.ApiScopes.SelectMany(r => r.UserClaims));
-			context.RequestedClaimTypes = claimsNames;
+
+			if (context.Caller != "UserInfoEndpoint")
+			{
+				//claimsNames.AddRange(context.RequestedResources.Resources.IdentityResources.SelectMany(r => r.UserClaims));
+				claimsNames.AddRange(context.RequestedResources.Resources.ApiResources.SelectMany(r => r.UserClaims));
+				claimsNames.AddRange(context.RequestedResources.Resources.ApiScopes.SelectMany(r => r.UserClaims));
+
+				context.RequestedClaimTypes = claimsNames;
+			}
 
 			var sub = context.Subject.GetSubjectId();
 			var user = Config.TestUsers.First(q => q.SubjectId == sub);
